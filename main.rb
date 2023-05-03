e61f9cdc6a031e3049f9b22a2639aca35ec57993
DifteR <its0black0magic@gmail.com>
2023-05-03 19:32:24 +0200

    build more methods

--- main.rb
+++ main.rb
@@ -6,18 +6,40 @@ class PlayingBoard
 
     def random_color_generator
         colors = %w[red blue green yellow black white]
-        random_color = colors.sample
+        return colors.sample
     end
 
     def computer_generated_code()
         i = 0
         while i<4
             @board_array[0][i] = random_color_generator
-            i +=1 
+            i += 1 
         end
     end
+
+    def check_color_and_position(j, i)
+        if @board_array[j][i] == @board_array [0][i]
+            return true
+        else return false
+        end
+    end
+
 end
 
 game = PlayingBoard.new()
 game.computer_generated_code
 puts game.board_array
+i = 1
+j = 0
+while(i<game.board_array.length)
+    puts "Enter four colors seperated by spaces, from left to right"
+    game.board_array[i] << gets.chomp.split(" ")
+    puts game.board_array
+    i += 1
+    while(j < game.board_array.length)
+        if game.check_color_and_position(j,i)
+            puts "A color and a position matches"
+        end
+        j += 1
+    end
+end
