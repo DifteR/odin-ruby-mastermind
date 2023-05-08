77e9b3aae0739c4f2ede258e8cbd87ad8d51d647
Tilen Marš <its0black0magic@gmail.com>
2023-05-08 06:31:22 +0000

    Fix some variable names

--- main.rb
+++ main.rb
@@ -37,19 +37,19 @@ end
 game = PlayingBoard.new()
 game.computer_generated_code
 puts game.board_array
-i = 1
-j = 0
-while(i<game.board_array.length)
+vrstica = 1
+stolpec = 0
+while(vrstica<game.board_array.length)
     puts "Enter four colors seperated by spaces, from left to right"
     #game.board_array[i] << gets.chomp.split(" ")
     userinput = gets.chomp.split(" ")
     puts game.board_array
-    game.input_colors(i, userinput)
-    while(j < game.board_array.length)
-        if game.check_color_and_position(j,i)
+    game.input_colors(vrstica, userinput)
+    while(stolpec < 4)
+        if game.check_color_and_position(vrstica,stolpec)
             puts "A color and a position matches"
         end
-        j += 1
+        stolpec += 1
     end
-    i += 1
+    vrstica += 1
 end
