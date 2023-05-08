97a8603930f76dd141ad40a13a966f5276e64aca
Tilen Marš <its0black0magic@gmail.com>
2023-05-08 06:25:02 +0000

    Progress

--- main.rb
+++ main.rb
@@ -17,8 +17,16 @@ class PlayingBoard
         end
     end
 
-    def check_color_and_position(j, i)
-        if @board_array[j][i] == @board_array [0][i]
+    def input_colors(vrstica, barve)
+        i = 0
+        while i<4
+            @board_array[vrstica][i] = barve[i]
+            i += 1
+        end
+    end
+
+    def check_color_and_position(vrstica, stolpec)
+        if @board_array[vrstica][stolpec] == @board_array [0][stolpec]
             return true
         else return false
         end
@@ -33,13 +41,15 @@ i = 1
 j = 0
 while(i<game.board_array.length)
     puts "Enter four colors seperated by spaces, from left to right"
-    game.board_array[i] << gets.chomp.split(" ")
+    #game.board_array[i] << gets.chomp.split(" ")
+    userinput = gets.chomp.split(" ")
     puts game.board_array
-    i += 1
+    game.input_colors(i, userinput)
     while(j < game.board_array.length)
         if game.check_color_and_position(j,i)
             puts "A color and a position matches"
         end
         j += 1
     end
+    i += 1
 end
