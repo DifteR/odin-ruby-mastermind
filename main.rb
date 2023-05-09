71f6b33e0dbeefdaf93bd17f1df60e5a6904ed84
Tilen Marš <its0black0magic@gmail.com>
2023-05-09 06:06:54 +0000

    Add check winner method and implement it

--- main.rb
+++ main.rb
@@ -32,6 +32,35 @@ class PlayingBoard
         end
     end
 
+    def check_only_color_match(vrstica)
+        stolpec = 0
+        rezultat = false
+        while(stolpec < 4)
+            stolpec2 = 0
+            while(stolpec2 < 4)
+                if @board_array[vrstica][stolpec2] == @board_array [0][stolpec]
+                    rezultat = true
+                #else rezultat = false
+                end
+                stolpec2 += 1
+            end
+            stolpec += 1
+        end
+        return rezultat
+    end
+
+    def check_winner(vrstica)
+        if @board_array[vrstica][0] == @board_array[0][0] &&
+           @board_array[vrstica][1] == @board_array[0][1] &&
+           @board_array[vrstica][2] == @board_array[0][2] &&
+           @board_array[vrstica][3] == @board_array[0][3]
+          true
+        else
+          false
+        end
+      end
+      
+
 end
 
 game = PlayingBoard.new()
@@ -44,11 +73,18 @@ while(vrstica<game.board_array.length)
     userinput = gets.chomp.split(" ")
     puts game.board_array
     game.input_colors(vrstica, userinput)
+    if(game.check_only_color_match(vrstica))
+        puts "A color matches"
+    end
     while(stolpec < 4)
         if game.check_color_and_position(vrstica,stolpec)
             puts "A color and a position matches"
         end
         stolpec += 1
+    end
+    if game.check_winner(vrstica)
+        puts "You won! The encoder gets #{vrstica} points!"
+        break
     end
     vrstica += 1
 end
